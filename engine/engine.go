4d658578af593a8fa5f8f96d4655f99a77fe796b
xiatian <819337877@qq.com>
2020-06-07 14:41:37 +0800

    10-单任务engine架构

--- engine/engine.go
+++ engine/engine.go
@@ -6,33 +6,30 @@ import (
 )
 
 func Run(seeds ...Request) {
-
+	// 把seeds放到Q队列
 	var requests []Request
-
 	for _, e := range seeds {
 		requests = append(requests, e)
 	}
-	// log.Print(requests)
 
 	for len(requests) > 0 {
-
+		// 取出Request(Url+ParseFunc)
 		r := requests[0]
-
-		// 爬取URL
 		requests = requests[1:]
+
+		// fetcher网页body(Url)
 		log.Printf("Fetching url:%s", r.Url)
 		body, err := fetcher.Fetch(r.Url)
-
 		if err != nil {
-			log.Printf("Fetch Error: %s", r.Url)
+			log.Printf("Fetch Error "+
+				"fetching url %s: %v", r.Url, err)
+			continue
 		}
 
+		// parse网页item(ParseFunc)
 		parseresult := r.ParseFunc(body)
-
-		requests = append(requests, parseresult.Requesrts...)
-		// log.Print("\n", requests, "\n")
-
-		for _, item := range parseresult.Items {
+		requests = append(requests, parseresult.Requesrts...) // 装载Requesrts
+		for _, item := range parseresult.Items {              // 打印Items
 			log.Printf("Got item:%s", item)
 		}
 	}
